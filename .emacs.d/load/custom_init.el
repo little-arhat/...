79b2b90e06703edab2b736e08a4aecb92113bb05
Roma Sokolov <sokolov.r.v@gmail.com>
2021-02-15 10:51:13 +0100

    custom

--- .emacs.d/load/custom_init.el
+++ .emacs.d/load/custom_init.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(highlight-parentheses-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
- '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(package-selected-packages '(kill-ring-search company telega use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
